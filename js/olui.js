b38e89ee87a5ccb229875871c1b2893f1444e4c4
Raphael Beer <rbeerdev@gmail.com>
2016-02-03 09:00:17 +0100

    Add info card link

--- js/olui.js
+++ js/olui.js
@@ -5,23 +5,44 @@ var OLUi = function() {
   this.machineData = {
     debian: {
       text: 'Debian is composed entirely of free software, most of which is under the GNU General Public License, and packaged by a group of individuals known as the Debian Project. Debian is a popular choice for personal computers and network servers, and has been used as a base for several other Linux distributions.',
-      titleSVG: './img/debian-title.svg'
+      titleSVG: './img/debian-title.svg',
+      link: {
+        url: 'http://www.debian.org/',
+        text: 'debian.org'
+      }
     },
     ubuntu: {
       text: 'Ubuntu comes with everything you need to run your organisation, school, home or enterprise. All the essential applications, like an office suite, browsers, email and media apps come pre-installed and thousands more games and applications are available in the Ubuntu software centre.',
-      titleSVG: './img/ubuntu-title.svg'
+      titleSVG: './img/ubuntu-title.svg',
+      link: {
+        url: 'http://www.ubuntu.com/desktop',
+        text: 'ubuntu.com'
+      }
     },
     elementary: {
       text: 'A fast and open replacement for Windows and OS X.',
-      titleSVG: './img/elementary-title.svg'
+      titleSVG: './img/elementary-title.svg',
+      link: {
+        url: 'https://elementary.io/',
+        text: 'elementary.io'
+      }
+
     },
     mint: {
       text: 'The purpose of Linux Mint is to produce a modern, elegant and comfortable operating system which is both powerful and easy to use.\n\nLinux Mint is the most popular desktop Linux distribution and the 3rd most widely used home operating system behind Microsoft Windows and Apple Mac OS.',
-      titleSVG: './img/mint-title.svg'
+      titleSVG: './img/mint-title.svg',
+      link: {
+        url: 'http://www.linuxmint.com/',
+        text: 'linuxmint.com'
+      }
     },
     manjaro: {
       text: 'Enjoy the simplicity.',
-      titleSVG: './img/manjaro-title.svg'
+      titleSVG: './img/manjaro-title.svg',
+      link: {
+        url: 'https://manjaro.github.io/',
+        text: 'manjaro.github.io'
+      }
     }
   }
   this.controls = {
@@ -88,8 +109,10 @@ OLUi.prototype.toggleInfoCard = function(show) {
 
 OLUi.prototype.setCardData = function(type, data) {
   var infoCard = this.controls.infoCard;
+  var linkHTML = '<a href="' + data.link.url + '" target="_blank">' + data.link.text + '</a>';
   if (type === 'info') {
     infoCard.textCard.children[0].innerText = data.text;
+    infoCard.textCard.children[1].innerHTML = linkHTML;
     infoCard.title.children[0].src = data.titleSVG;
   }
 };
